18057cf2ad64f623f2f4e8d13ff29a5473123112
hovelopin <kimhojin3714@naver.com>
2023-04-02 22:15:43 +0900

    feat : add bfs

---
+++ packages/common/lib/structure/bfs.ts
@@ -0,0 +1,19 @@
+type Graph = {
+  [key: string] : string[]
+}
+
+export function bfs(graph:Graph, startNode:string){
+  const visited:string[] = []; // 탐색을 마친 노드들
+  let needVisit:string[] = []; // 탐색해야할 노드들
+
+  needVisit.push(startNode); // 노드 탐색 시작
+
+  while (needVisit.length !== 0) { // 탐색해야할 노드가 남아있다면
+    const node = needVisit.shift() as string; // queue이기 때문에 선입선출, shift()를 사용한다.
+    if (!visited.includes(node)) { // 해당 노드가 탐색된 적 없다면
+      visited.push(node); 
+      needVisit = [...needVisit, ...graph[node]];
+    }
+  }
+  return visited;
+};
